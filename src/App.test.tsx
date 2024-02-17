93f08559ad51d4e24dfe338d46a24746179e6b37
Sage Neil Smith <sage.neil.smith@gmail.com>
2024-02-17 16:33:21 -0700

    Add dependabot and tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders user management', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const userManagement = screen.getByText(/User Management/i);
+  expect(userManagement).toBeInTheDocument();
+});
+
+test('renders citation management', () => {
+  render(<App />);
+  const citationManagement = screen.getByText(/Citation Management/i);
+  expect(citationManagement).toBeInTheDocument();
 });
